38a289e39e5af272fb24b4cd799d0dd6d26d5c82
xiayuhang707 <m13131751927@163.com>
2017-12-13 20:18:59 +0800

    同步双选会

--- shuangxuan/js/script.js
+++ shuangxuan/js/script.js
@@ -1,15 +1,39 @@
 
 var swiperContainer = new Swiper('.swiper-container', {
-    effect: 'cube',
+    slidesPerView: 1,
     direction: 'vertical',
-    grabCursor: true,
-    cubeEffect: {
-        shadow: true,
-        slideShadows: true,
-        shadowOffset: 20,
-        shadowScale: 0.94
-    },
+    spaceBetween: 30,
+    loop: true,
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
+
+
+//背景音乐
+var mp3=document.querySelector("#mp3");
+var mp3btn=document.querySelector("#mp3btn");
+
+//暂停音乐按钮
+//mp3Btn.style.animationPlayState = 'paused'
+//旋转音乐按钮
+//mp3Btn.style.animationPlayState = 'running'
+
+mp3btn.onclick = function () {
+    if (mp3.paused) {
+        //开始旋转
+        mp3btn.style.animationPlayState = 'running';
+        //开始音频
+        mp3.play();
+    } else {
+        //暂停旋转
+        mp3btn.style.animationPlayState = 'paused';
+        //暂停音频
+        mp3.pause();
+    }
+}
