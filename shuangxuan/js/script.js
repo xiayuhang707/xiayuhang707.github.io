b23f229601dc903fb6328bd802c6e4f54da5e579
xiayuhang707 <m13131751927@163.com>
2017-12-15 10:50:17 +0800

    同步双选会

--- shuangxuan/js/script.js
+++ shuangxuan/js/script.js
@@ -12,6 +12,21 @@ var swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    pagination: {
+        el: '.swiper-pagination',
+        //小圆点是否可以点击
+        clickable: true
+    },
+    //加入动画效果
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
 
 
