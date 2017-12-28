bcb7ef70ddad084397264045b95e11469951768b
P\w <1659098863@.qq.com>
2017-12-28 14:58:45 +0800

    新增animation

---
+++ animation/vue 循环/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lenovo on 17/12/26 026.
+ */
+new Vue({
+    el:"#app",
+    data:{
+        lists:[
+            {
+                p:'练习'
+            },
+            {
+                p:'训练'
+            }
+        ]
+    }
+})
